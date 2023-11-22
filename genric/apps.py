eecdf690da8f8ad1d432d36827397bac244820c9
ssairaj99 <ssairajsahoo23@gmail.com>
2023-11-22 22:25:10 +0530

    first commit

---
+++ genric/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GenricConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'genric'
---
+++ genric/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def india(request):
+    return render(request,'india.html')
